cb6ac84975549cf528b3e1b1c7a043ada1a1b0d2
alexmitroff <alexmitroff@gmail.com>
2021-02-05 22:34:07 +0300

    Update setup and snapcraft

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='pogost-telegram-bot',
-   version='0.1',
+   version='1.0',
    description='Simplified telegram bot',
    author='Aleksei Mitrofanov',
    author_email='alexmitroff@gmail.com',
